f836c190ea8bd98ff39472e3125126df2c8b08e3
isissi <isissi_525@hotmail.com>
2021-11-17 21:53:40 -0700

    initial push

---
+++ fetcher.js
@@ -0,0 +1,23 @@
+const request = require("request");
+const Buffter = require("buffer");
+const fs = require("fs");
+let args = process.argv.slice(2);
+const url = args[0];
+const path = args[1];
+
+request(url, (error, response, body) => {
+  fs.writeFile(path, body, (error) => {
+    if (error) {
+      console.log(`Fail to save file: ${error}`);
+      return;
+    }
+
+    fs.stat(path, (err, stats) => {
+      if (error) {
+        console.log(`Fail to read file: ${error}`);
+        return;
+      }
+      console.log(`Downloaded and saved ${stats.size} bytes to ${path}`);
+    });
+  });
+});
